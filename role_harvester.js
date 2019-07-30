61b9835c518f85ee9e1979e958c804f859e31474
Dustin Brown <13907842+dustinlbrown@users.noreply.github.com>
2019-07-30 16:03:56 -0400

    Update role_harvester.js

--- role_harvester.js
+++ role_harvester.js
@@ -3,22 +3,24 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-    	if (typeof creep.memory.AssignedSource === 'undefined' ) {
+    	var sources = creep.room.find(FIND_SOURCES)
+
+    	if (typeof creep.memory.assignedSource === 'undefined' ) {
     		console.log(creep.name + ' does not have an assigned source.');
-			
-			for(var source in creep.room.find(FIND_SOURCES)) {
+			for(var source in sources) {
 				console.log(source);
-				if (_.filter(Game.creeps,(creep) => creep.memory.AssignedSource == source).length == '0' ){
-					console.log('Assigning Creep to source ' + source)
-        			creep.memory.AssignedSource = source;
+				if (_.filter(Game.creeps,(creep) => creep.memory.assignedSource == source).length == '0' ){
+					console.log('Assigning ' + creep.name + ' to source ' + source)
+        			creep.memory.assignedSource = source;
         			break;
         		}	
 	        }
         }
 
+
 		if(creep.carry.energy < creep.carryCapacity) {
-            if(creep.harvest(sources[creep.memory.AssignedSource]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[creep.memory.AssignedSource], {visualizePathStyle: {stroke: '#ffaa00'}});
+            if(creep.harvest(sources[creep.memory.assignedSource]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[creep.memory.assignedSource], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         
