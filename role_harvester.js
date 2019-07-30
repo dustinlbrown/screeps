aff78a371abac596e7215356d1692ecef1103165
Dustin Brown <13907842+dustinlbrown@users.noreply.github.com>
2019-07-30 16:51:47 -0400

    Update role_harvester.js

--- role_harvester.js
+++ role_harvester.js
@@ -18,25 +18,8 @@ var roleHarvester = {
 	        }
         }
 
-
-		if(creep.carry.energy < creep.carryCapacity) {
-            if(creep.harvest(sources[creep.memory.assignedSource]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[creep.memory.assignedSource], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-        }
-        
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.energy < structure.energyCapacity;
-                    }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
+        if(creep.harvest(sources[creep.memory.assignedSource]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[creep.memory.assignedSource], {visualizePathStyle: {stroke: '#ffaa00'}});
         }
 	}
 };
