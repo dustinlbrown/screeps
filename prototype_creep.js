43efa2a277959f27262c4204be657dc1b9d9e95b
Dustin Brown <13907842+dustinlbrown@users.noreply.github.com>
2019-07-31 10:46:31 -0400

    add withdrawEnergy to creep prototype

--- prototype_creep.js
+++ prototype_creep.js
@@ -44,6 +44,65 @@ Creep.prototype.harvestEnergy = function() {
     }
 }
 
+Creep.prototype.withdrawEnergy = function() {
+    var structures = this.room.find(FIND_MY_STRUCTURES);
+    var target; //undefined
+    var hasStorage = false;
+    var link = _.filter(structures,{structureType: STRUCTURE_LINK});
+    if (link.length){
+        for (var i in link){
+            if (this.pos.isNearTo(link[i])){
+                if(link[i].energy > 0){
+                    target = link[i];
+                }
+                break;
+            }
+        }
+    }
+
+    if(typeof target === 'undefined'){
+        //var storage = _.filter(structures,{structureType: STRUCTURE_STORAGE});
+        var storage = this.room.storage;
+
+        if (typeof storage !== 'undefined' ){
+            hasStorage = true;
+            if (storage.store.energy > 0) {
+                target = storage;
+            }
+        }
+
+    }
+
+    //We don't have storage, or it's empty.
+    if (typeof target === 'undefined' && !hasStorage) {
+        var extensions = _.filter(structures, {structureType: STRUCTURE_EXTENSION});
+
+        var filledExtensions = _.filter(extensions, function (r) {
+            return r.energy > 0;
+        });
+        if (filledExtensions.length) {
+            target = this.pos.findClosestByRange(filledExtensions);
+        }
+    }
+    //if we still don't have a target and we don't have too many extensions, we'll borrow from the spawn
+    if (typeof target === 'undefined' && _.filter(structures, {structureType: STRUCTURE_EXTENSION}).length < 1 && !hasStorage) {
+        var spawns = _.filter(structures, {structureType: STRUCTURE_SPAWN});
+        if (spawns.length){
+            target = spawns[0];
+        }
+
+        //first spawn is fine as we'll never take from it by the time we have 2
+    }
+
+    if (typeof target !== 'undefined') { //todo figure out why we move to a target we don't have....
+        this.moveTo(target);
+        this.withdraw(target,RESOURCE_ENERGY);
+        this.memory.withdrawalSource = target.structureType;
+        if(this.carry.energy === this.carryCapacity){this.memory.action = ACTION.CONSTRUCTION;}
+    }
+};
+
+
 Creep.prototype.depositEnergy = function() {
     var target = undefined;
 
