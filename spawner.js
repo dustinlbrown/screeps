174533fa9d1bdbbc6a834b76ac44c0029bfa0901
Dustin Brown <13907842+dustinlbrown@users.noreply.github.com>
2019-07-31 15:15:09 -0400

    refactoring of the spawner code

--- spawner.js
+++ spawner.js
@@ -1,6 +1,9 @@
 var spawner = {
 
     run: function (){
+
+		var CREEP_ROLES = ['builder','carrier','harvester','upgraders']
+
 		var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
 	    var carriers = _.filter(Game.creeps, (creep) => creep.memory.role == 'carrier');
 	    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
@@ -12,38 +15,47 @@ var spawner = {
 	    var idealHarvesters = Game.spawns['Spawn1'].room.find(FIND_SOURCES).length;
 	    var idealUpgraders = 1;
 
-	    if (carriers.length < idealCarriers) {
-	        var newName = 'Carrier' + Game.time;
-	        console.log('Spawning new carrier: ' + newName);
-	        Game.spawns['Spawn1'].spawnCreep([CARRY, CARRY, MOVE, MOVE], newName, 
-	        	{memory: {role: 'carrier'}});
-	    }
 
-	    if (harvesters.length < idealHarvesters) {
-	        var newName = 'Harvester' + Game.time;
-	        console.log('Spawning new harvester: ' + newName);
-	        Game.spawns['Spawn1'].spawnCreep([WORK, WORK, MOVE], newName, 
-	            {memory: {role: 'harvester'}});
-	    }
-	    
-	    if (builders.length < idealBuilders) {
-	        var newName = 'Builder' + Game.time;
-	        console.log('Spawning new builder: ' + newName);
-	        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName, 
-	            {memory: {role: 'builder'}});
-	    }
-	    
-	    if (upgraders.length < idealUpgraders) {
-	        var newName = 'Upgrader' + Game.time;
-	        console.log('Spawning new upgrader: ' + newName);
-	        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName, 
-	            {memory: {role: 'upgrader'}});
-	    }
-	    
-	    if (Game.spawns['Spawn1'].spawning) { 
+
+		if(!Game.spawns['Spawn1'].spawning){
+
+			var creepBody = []
+			var creepRole = '';
+
+			if (carriers.length < idealCarriers) {
+				creepBody = [];
+				creepBody.push(CARRY);
+				creepBody.push(CARRY);
+				creepBody.push(MOVE);
+				creepBody.push(MOVE);
+			}else if (harvesters.length < idealHarvesters) {
+				creepBody = [];
+				creepBody.push(WORK);
+				creepBody.push(WORK);
+				creepBody.push(MOVE);
+			}else if (builders.length < idealBuilders) {
+				creepBody = [];
+				creepBody.push(WORK);
+				creepBody.push(WORK);				
+				creepBody.push(CARRY);
+				creepBody.push(MOVE);
+			}else if (upgraders.length < idealUpgraders) {
+				creepBody = [];
+				creepBody.push(WORK);
+				creepBody.push(WORK);				
+				creepBody.push(CARRY);
+				creepBody.push(MOVE);
+			}
+			
+			
+			Game.spawns['Spawn1'].spawnCreep(creepBody, creepRole + Game.time, 
+				{memory: {role: creepRole}});
+			console.log('Spawning new ' + creepRole + ': ' + newName);
+
+		}else { 
 	        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
 	        Game.spawns['Spawn1'].room.visual.text(
-	            'SPAWNING:' + spawningCreep.memory.role,
+	            'SPAWNING: ' + spawningCreep.memory.role,
 	            Game.spawns['Spawn1'].pos.x + 1, 
 	            Game.spawns['Spawn1'].pos.y, 
 	            {align: 'left', opacity: 0.8});
