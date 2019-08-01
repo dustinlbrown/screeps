c1d58199da0f7cb3b881ad7596575fcae98dbd44
Dustin Brown <13907842+dustinlbrown@users.noreply.github.com>
2019-08-01 00:55:51 -0400

    keeping the machine running

--- spawner.js
+++ spawner.js
@@ -73,7 +73,7 @@ function getCreepBody(role){
 	}else if (role == 'carrier'){
 		opts = {body: {"move": 1, "carry": 1},exact: false};
 	}else if (role == 'harvester'){
-		opts = {body: {"work": 2, "move": 2, "work": 3, "move": 3},exact: true};
+		opts = {body: {"work": 2, "move": 2, "work": 2, "move": 2},exact: true};
 	}else if (role == 'upgrader'){
 		opts = {body: {"work": 2, "move": 1, "carry": 1},exact: false};
 	}
